5ddaaed64daa94e458e7db2dc6d5394009672264
Alex Buly <asbuly@icloud.com>
2023-07-30 14:45:58 -0400

    Add operation functions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // operator functions 
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+console.log(add);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
+
 // takes in numbers to run the calculator with one function 
 // calculator buttons
 // populate display 
