75e2379238deab6e2a0167ced3ba10c45c117700
Alex Buly <asbuly@icloud.com>
2023-07-30 14:02:14 -0400

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,6 @@
+// operator functions 
+// takes in numbers to run the calculator with one function 
+// calculator buttons
+// populate display 
+// use result and new number as new number pair 
+// works when dividing by 0 
