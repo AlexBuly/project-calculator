33e94e98348f3225d1cf8d51572226b121e23092
Alex Buly <asbuly@icloud.com>
2023-08-08 16:26:05 -0400

    Edit script.js
    When the user clicks a number button (except zero) the appropriate
    number displays on the screen by adding the number to an array. Clear
    empties the array.

--- script.js
+++ script.js
@@ -4,48 +4,58 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-console.log(add);
-console.log(subtract);
-console.log(multiply);
-console.log(divide);
-
-// takes in numbers to run the calculator with one function 
-const num1 = operate;
-const op = operate;
-const num2 = operate;
-
-function operate(num1, op, num2) {
-        return op === "+" ? add(num1, num2)
-        : op === "-" ? subtract(num1, num2)
-        : op === "*" ? multiply(num1, num2)
-        : op === "/" ? divide(num1, num2)
-        : null; 
- }
-
- const result = operate(num1, op, num2);
- console.log(result);
+add();
+subtract();
+multiply();
+divide();
 
  const display = document.querySelector(".display");
+ const displayArray = [];
+ display.textContent = 0;
 
 // calculator buttons
 const calculator = document.querySelector(".calculator");
-const opLabels = ["/", "*", "-", "*", "="];
+const opLabels = ["/", "*", "-", "+", "="];
 
 const operateBtns = document.querySelectorAll(".operateBtns");
 operateBtns.forEach((btn, index) => {
     btn.textContent = opLabels[index];
+    btn.addEventListener("click", () => {
+        op = opLabels[index];
+    })
 });
 
 const numLabels = [7, 8, 9, 4, 5, 6, 1, 2, 3];
 const leftBtns = document.querySelectorAll(".leftBtns");
 leftBtns.forEach((btn, index) => {
     btn.textContent = numLabels[index];
+    btn.addEventListener("click", () => {
+        num1 = numLabels[index];
+        num2 = numLabels[index];
+        displayArray.push(num1);
+        display.textContent = displayArray.join('');
+    });
 });
 
-leftBtns.addEventListener("click", () => {
-    num1 = numLabels.forEach((num) => numLabels[num]);
-    //display.textContent = function for display 
+console.log(displayArray);
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    displayArray.length = 0;
+    display.textContent = 0;
+
 });
+
+// takes in numbers to run the calculator with one function 
+function operate(num1, op, num2) {
+    return op === "+" ? add(num1, num2)
+    : op === "-" ? subtract(num1, num2)
+    : op === "*" ? multiply(num1, num2)
+    : op === "/" ? divide(num1, num2)
+    : 0; 
+}
+
+
 
 // populate display
 // use result and new number as new number pair 
